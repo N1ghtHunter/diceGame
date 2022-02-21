5bd4101498db3a65597a8cb6e4962a35149b67d9
N1ghtHunter <93228608+N1ghtHunter@users.noreply.github.com>
2022-02-22 00:07:50 +0200

    updater constant click

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function diceGame() {
 var randomNumber1 = Math.floor((Math.random() * 5 + 1))
 var randomNumber2 = Math.floor((Math.random() * 5 + 1))
-
+var header = document.querySelector("h1");
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
 document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("button").textContent = "🚩Player 1 wins!"
+  header.textContent = "🚩Player 1 wins!"
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("button").textContent = "Player 2 wins!🚩"
+  header.textContent = "Player 2 wins!🚩"
 } else {
-  document.querySelector("button").textContent = "DRAW!"
+  header.textContent = "DRAW!"
 }
+header.classList.remove("hidden");
 }
